0662c52442bc0dba916af042ecc74cda596c9926
Kurt Morrow <kurt.morrow@gmail.com>
2020-07-10 13:42:33 -0700

    updated for deployment

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/first_capstone',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/first_capstone_test'
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/first_capstone_test',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api"
   }
